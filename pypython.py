a2eb63b1ebfc7d3f5aae6037d97e4beefaa6545c
Dwayne Dantes <139754226+gomeo05@users.noreply.github.com>
2023-09-02 14:52:48 +0700

    Update pypython.py

--- pypython.py
+++ pypython.py
@@ -4,3 +4,12 @@ find
 #gm
 #read it
 
+Launching on 
+@Scroll_ZKP
+ Sepolia Testnet!
+
+Scroll is a fully EVM-compatible zk-Rollup and that will be our new voyage in the #layer2 ecosystem.
+
+The brand new SpaceFi 2.0 is still in cooking. Stay tuned👀
+
+🏆 Explore the Last #Scroll Testnet
