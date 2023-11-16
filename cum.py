29e0474a93dc3d4efb670bca160b0b5a98665381
Dwayne Dantes <139754226+gomeo05@users.noreply.github.com>
2023-11-16 11:18:21 +0700

    Delete cum.py

--- cum.py
+++
@@ -1,4 +0,0 @@
-import pyth.py
-import noew.json
-#"I was shouting on the radio saying: 'What the hell happened?' We were nowhere, but the team said everyone was struggling. I have never seen such an influence on car balance."
-
