7ed2825d0cbae6d1824e8f094906c889ecd7a62e
Dwayne Dantes <139754226+gomeo05@users.noreply.github.com>
2023-11-27 14:32:21 +0700

    Update import.py

--- import.py
+++ import.py
@@ -12,3 +12,14 @@ Scroll is a fully EVM-compatible zk-Rollup and that will be our new voyage in th
 The brand new SpaceFi 2.0 is still in cooking. Stay tuned👀
 
 🏆 Explore the Last #Scroll Testnet
+Former England manager Terry Venables has died at the age of 80 after a long illness.
+
+Venables managed England from 1994 to 1996, most notably leading them to the semi-finals of Euro 96 on home soil.
+
+A former England player, he also managed Barcelona and Tottenham.
+
+"We are totally devastated by the loss of a wonderful husband and father who passed away peacefully yesterday after a long illness," read a family statement.
+
+"We would ask that privacy be given at this incredibly sad time to allow us to mourn the loss of this lovely man who we were so lucky to have had in our lives."
+
+Venables won La Liga and reached the European Cup final with Barcelona, and lifted the FA Cup with Tottenham.
